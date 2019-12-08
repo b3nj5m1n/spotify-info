0f8a71ecfa09b7867868a07972161f288f434404
b3nj5m1n <47924309+b3nj5m1n@users.noreply.github.com>
2019-12-08 12:34:20 +0100

    Process handleing
    Application now able to get process of active window

--- spotify-info/Form1.Designer.cs
+++ spotify-info/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace spotify_info
 {
-    partial class Form1
+    partial class form
     {
         /// <summary>
         /// Erforderliche Designervariable.
@@ -28,13 +28,51 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn_getProcess = new System.Windows.Forms.Button();
+            this.prog_getspotifyprocess = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // btn_getProcess
+            // 
+            this.btn_getProcess.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn_getProcess.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btn_getProcess.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_getProcess.ForeColor = System.Drawing.Color.White;
+            this.btn_getProcess.Location = new System.Drawing.Point(0, 0);
+            this.btn_getProcess.Name = "btn_getProcess";
+            this.btn_getProcess.Size = new System.Drawing.Size(800, 76);
+            this.btn_getProcess.TabIndex = 0;
+            this.btn_getProcess.Text = "Get spotify process";
+            this.btn_getProcess.UseVisualStyleBackColor = true;
+            this.btn_getProcess.Click += new System.EventHandler(this.btn_getProcess_Click);
+            // 
+            // prog_getspotifyprocess
+            // 
+            this.prog_getspotifyprocess.Dock = System.Windows.Forms.DockStyle.Top;
+            this.prog_getspotifyprocess.Location = new System.Drawing.Point(0, 76);
+            this.prog_getspotifyprocess.Name = "prog_getspotifyprocess";
+            this.prog_getspotifyprocess.Size = new System.Drawing.Size(800, 23);
+            this.prog_getspotifyprocess.TabIndex = 1;
+            // 
+            // form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.prog_getspotifyprocess);
+            this.Controls.Add(this.btn_getProcess);
+            this.Name = "form";
+            this.Text = "Spotify Info";
+            this.Load += new System.EventHandler(this.form_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_getProcess;
+        private System.Windows.Forms.ProgressBar prog_getspotifyprocess;
     }
 }
 
--- spotify-info/Form1.cs
+++ spotify-info/Form1.cs
@@ -4,17 +4,75 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace spotify_info
 {
-    public partial class Form1 : Form
+    public partial class form : Form
     {
-        public Form1()
+        public form()
         {
             InitializeComponent();
+        }
+
+        private void form_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        #region Process/Window
+        // Get the process of the window currently in focus
+        [DllImport("user32.dll")]
+        static extern IntPtr GetForegroundWindow();
+        // Get the window name of a process
+        [DllImport("user32.dll")]
+        static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
+        // Handle to store the process handle of the spotify process
+        public IntPtr handle;
+        // Get the window name of the spotify process
+        private string GetWindowTitle()
+        {
+            const int nChars = 256;
+            StringBuilder Buff = new StringBuilder(nChars);
+
+            if (GetWindowText(handle, Buff, nChars) > 0)
+            {
+                return Buff.ToString();
+            }
+            return null;
+        }
+        #endregion
+
+        // When pressed, you have 5 seconds to focus the spotif application window
+        private void btn_getProcess_Click(object sender, EventArgs e)
+        {
+            Task getProcessTask = new Task(get_Process);
+            getProcessTask.Start();
+        }
+
+        private void get_Process()
+        {
+            // Delay to wait for to focus the spotify application in ms
+            int delay = 5000;
+            // Wait for delay
+            for (int i = 0; i < delay; i+=delay/100)
+            {
+                Thread.Sleep(delay/100);
+                prog_getspotifyprocess.Invoke((MethodInvoker)delegate {
+                    prog_getspotifyprocess.Value += 1;
+                });
+            }
+            
+            // Get process handle of active window
+            handle = GetForegroundWindow();
+            // Change the text of the button to the window name
+            btn_getProcess.Invoke((MethodInvoker)delegate {
+                btn_getProcess.Text = "Task name: " + GetWindowTitle();
+            });
         }
     }
 }
